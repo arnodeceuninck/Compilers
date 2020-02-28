767b07931149de7c9f54aff0ea2e126c26d15a38
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-02-28 17:59:07 +0100

    fixed errors in main

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ from gen import cParser
 
 def main(argv):
     input_stream = FileStream(argv[1])
-    lexer = cLexer(input_stream)
+    lexer = cLexer.cLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = cParser(stream)
+    parser = cParser.cParser(stream)
     tree = parser.start_rule()
-
-
 
 if __name__ == '__main__':
     main(sys.argv)
