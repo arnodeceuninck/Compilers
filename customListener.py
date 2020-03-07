5c7a5a07789509c6c2592ac5e6a5d32b991b41f6
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2020-03-07 20:20:32 +0100

    Creating an AST for only numbers (e.g. 1;)

--- customListener.py
+++ customListener.py
@@ -2,27 +2,36 @@ from antlr4 import *
 from AST import AST
 from gen.cParser import cParser
 
-PREVIOUS_CTX = None
 
 # This class defines a complete listener for a parse tree produced by cParser.
 class customListener(ParseTreeListener):
 
+    finalTree = None
+    previousTree = None
+
+    def __init__(self):
+        self.previousTree = None
+        self.finalTree = None
+
     # Enter a parse tree produced by cParser#start_rule.
     def enterStart_rule(self, ctx: cParser.Start_ruleContext):
-
+        print("enter Start")
         pass
 
     # Exit a parse tree produced by cParser#start_rule.
     def exitStart_rule(self, ctx: cParser.Start_ruleContext):
-        print("kaas")
+        print("exit Start")
+        self.finalTree = self.previousTree
         pass
 
     # Enter a parse tree produced by cParser#operation.
     def enterOperation(self, ctx: cParser.OperationContext):
+        print("Enter Operation")
         pass
 
     # Exit a parse tree produced by cParser#operation.
     def exitOperation(self, ctx: cParser.OperationContext):
+        print("Exit operation")
         pass
 
     # Enter a parse tree produced by cParser#operation_logic_or.
@@ -59,18 +68,23 @@ class customListener(ParseTreeListener):
 
     # Enter a parse tree produced by cParser#operation_plus_minus.
     def enterOperation_plus_minus(self, ctx: cParser.Operation_plus_minusContext):
+        print("Enter +-")
         pass
 
     # Exit a parse tree produced by cParser#operation_plus_minus.
     def exitOperation_plus_minus(self, ctx: cParser.Operation_plus_minusContext):
+        print("Exit +-")
+
         pass
 
     # Enter a parse tree produced by cParser#operation_mult_div.
     def enterOperation_mult_div(self, ctx: cParser.Operation_mult_divContext):
+        print("Enter */")
         pass
 
     # Exit a parse tree produced by cParser#operation_mult_div.
     def exitOperation_mult_div(self, ctx: cParser.Operation_mult_divContext):
+        print("Exit */")
         pass
 
     # Enter a parse tree produced by cParser#operation_unary_plus_minus_not.
@@ -83,16 +97,23 @@ class customListener(ParseTreeListener):
 
     # Enter a parse tree produced by cParser#operation_brackets.
     def enterOperation_brackets(self, ctx: cParser.Operation_bracketsContext):
+        print("enter (")
+        if ctx.ID():
+            print(ctx.getText())
+            self.previousTree = AST(value=ctx.getText())
         pass
 
     # Exit a parse tree produced by cParser#operation_brackets.
     def exitOperation_brackets(self, ctx: cParser.Operation_bracketsContext):
+        print("exit )")
         pass
 
     # Enter a parse tree produced by cParser#identifier.
     def enterIdentifier(self, ctx: cParser.IdentifierContext):
+        print("Enter identifier")
         pass
 
     # Exit a parse tree produced by cParser#identifier.
     def exitIdentifier(self, ctx: cParser.IdentifierContext):
+        print("Exit identifier")
         pass
