c54b2d5fea8bdf9d37c549971d8923c8f6466814
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-04-19 23:42:59 +0200

    fixed tests?

--- tests/test.py
+++ tests/test.py
@@ -18,15 +18,15 @@ class MyTestCase(unittest.TestCase):
         # Reset node id because it causes errors in llvm creation
         AST.reset()
 
-        input_file: str = "input/" + test_name + ".c"
-        output_file: str = "output/" + test_name + ".dot"
-        output_file_ll: str = "output/" + test_name + ".ll"
-        output_file_folded: str = "output/" + test_name + ".folded.dot"
-        output_file_folded_ll: str = "output/" + test_name + ".folded.ll"
-        expected_output_file: str = "expected_output/" + test_name + ".dot"
-        expected_output_file_ll: str = "expected_output/" + test_name + ".ll"
-        expected_output_file_folded: str = "expected_output/" + test_name + ".folded.dot"
-        expected_output_file_folded_ll: str = "expected_output/" + test_name + ".folded.ll"
+        input_file: str = "test/input/" + test_name + ".c"
+        output_file: str = "test/output/" + test_name + ".dot"
+        output_file_ll: str = "test/output/" + test_name + ".ll"
+        output_file_folded: str = "test/output/" + test_name + ".folded.dot"
+        output_file_folded_ll: str = "test/output/" + test_name + ".folded.ll"
+        expected_output_file: str = "test/expected_output/" + test_name + ".dot"
+        expected_output_file_ll: str = "test/expected_output/" + test_name + ".ll"
+        expected_output_file_folded: str = "test/expected_output/" + test_name + ".folded.dot"
+        expected_output_file_folded_ll: str = "test/expected_output/" + test_name + ".folded.ll"
 
         tree: AST = compile(input_file, catch_error=catch_errors)
         if tree:
