bd5a071e025de9a00ffba3797af811387a266131
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-16 09:33:56 +0200

    Fixed wrong spim output

--- src/MIPS/run_mips.sh
+++ src/MIPS/run_mips.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # sudo apt-get install spim
 # No idea what delayed branches and loads does
-spim -delayed_branches -delayed_loads -file $1
+#spim -delayed_branches -delayed_loads -file $1
+spim -file $1
