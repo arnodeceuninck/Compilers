7ca166390d6c5c5f591d94d75f12b42efc69d767
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-23 13:48:28 +0200

    Removed printf4

--- tests/CorrectCodeTests.py
+++ tests/CorrectCodeTests.py
@@ -113,9 +113,6 @@ class CorrectCodeTests(unittest.TestCase):
     def test_printf3(self):
         self.help_test()
 
-    def test_printf4(self):
-        self.help_test()
-
     def test_scanf1(self):
         return
         self.help_test()
--- tests/CorrectMipsCodeTests.py
+++ tests/CorrectMipsCodeTests.py
@@ -161,9 +161,6 @@ class CorrectMipsCodeTests(unittest.TestCase):
     def test_printf3(self):
         self.help_test()
 
-    def test_printf4(self):
-        self.help_test()
-
     def test_scanf1(self):
         return
         self.help_test()
