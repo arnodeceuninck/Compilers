be6569d9bbd1288e9c328f56dbba1db160979f82
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-23 14:46:56 +0200

    removed lib

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 mkdir "tests/output"
 #mkdir "output"
-antlr4 -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
-antlr4 -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
+antlr4 -o gen -listener -visitor -Dlanguage=Python3 input/llvm.g4
+antlr4 -o gen_llvm -listener -visitor -Dlanguage=Python3 input/c.g4
 python3 -m unittest tests/CorrectCodeTests.py tests/CorrectMipsCodeTests.py tests/SemanticErrorTests.py
