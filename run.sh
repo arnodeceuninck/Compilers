01f11fa0fbed3852229e5cedcb170470a3f7c77d
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-05-23 18:13:10 +0200

    fixed run scripts and made some module changes

--- run.sh
+++ run.sh
@@ -1,13 +1,33 @@
-#export PYTHONPATH=$PYTHONPATH:gen
-#antlr4 -Dlanguage=Python3 -visitor -Xexact-output-dir -o gen input/c.g4
-#python3 src/main.py input/test.c
+#!/bin/bash
 
-# Asumes you're in the folder compilers
-#mkdir "tests/output"
-wget https://www.antlr.org/download/antlr-4.8-complete.jar
+# Assumes you're in the folder compilers
+export PYTHONPATH=$PYTHONPATH:src
+
+# Downloads the jar if it does not already exist in the folder
+if [[ ! -f "antlr-4.8-complete.jar" ]]; then
+  wget https://www.antlr.org/download/antlr-4.8-complete.jar
+fi
+# Remove the generated grammar from the folders
+if [[ -d "gen" ]]; then
+  rm -rf gen/*
+fi
+if [[ -d "llvm_gen" ]]; then
+  rm -rf llvm_gen/*
+fi
+
+# Generate the parser to parse our c and llvm files
 java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 input/llvm.g4
 java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 input/c.g4
+# Move the directories to the gen folders
 mv gen_llvm/input/* gen_llvm
 mv gen/input/* gen
-mkdir "output"
-python3 src/main.py input/y.c # y.c is the file we're going to compile
+# Removes the empty input directories
+rmdir gen*/input
+
+# make the output folder
+if [[ ! -d "output" ]]; then
+  mkdir "output"
+fi
+
+# y.c is the file we're going to compile, but can be any file
+python3 src/main.py input/y.c
--- test.sh
+++ test.sh
@@ -1,8 +1,32 @@
-wget https://www.antlr.org/download/antlr-4.8-complete.jar
+#!/bin/bash
+
+# Ensures that you can compile the file correctly
+export PYTHONPATH=$PYTHONPATH:src
+
+# Downloads the jar if it does not already exist in the folder
+if [[ ! -f "antlr-4.8-complete.jar" ]]; then
+  wget https://www.antlr.org/download/antlr-4.8-complete.jar
+fi
+# Remove the generated grammar from the folders
+if [[ -d "gen" ]]; then
+  rm -rf gen/*
+fi
+if [[ -d "llvm_gen" ]]; then
+  rm -rf llvm_gen/*
+fi
+
+# Generate the parser to parse our c and llvm files
 java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 input/llvm.g4
 java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 input/c.g4
+# Move the directories to the gen folders
 mv gen_llvm/input/* gen_llvm
 mv gen/input/* gen
-mkdir "tests/output"
-python3 src/main.py input/y.c # y.c is the file we're going to compile
+rmdir gen*/input
+
+# make the output folder for the tests
+if [[ ! -d "tests/output" ]]; then
+  mkdir "tests/output"
+fi
+
+# Runs all the tests aka benchmarks with pythons unittests
 python3 -m unittest tests/CorrectCodeTests.py tests/CorrectMipsCodeTests.py tests/SemanticErrorTests.py
