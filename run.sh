0b2109f0d5c03b2fd235aff655d1021968ec9c2f
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-23 15:00:05 +0200

    Removed lib input

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 # Asumes you're in the folder compilers
 #mkdir "tests/output"
 wget https://www.antlr.org/download/antlr-4.8-complete.jar
-java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
-java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
+java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 input/llvm.g4
+java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 input/c.g4
 mkdir "output"
 python3 src/main.py input/y.c # y.c is the file we're going to compile
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 wget https://www.antlr.org/download/antlr-4.8-complete.jar
-java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
-java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
+java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 input/llvm.g4
+java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 input/c.g4
 mkdir "tests/output"
 python3 src/main.py input/y.c # y.c is the file we're going to compile
 python3 -m unittest tests/CorrectCodeTests.py tests/CorrectMipsCodeTests.py tests/SemanticErrorTests.py
