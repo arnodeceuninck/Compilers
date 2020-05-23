bccfd799a833f53d8a436463232c35f84f16c86d
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-23 14:57:36 +0200

    Compile antlr from the jar

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
-# Asumes you're in the folder compiolers
-mkdir "tests/output"
+wget https://www.antlr.org/download/antlr-4.8-complete.jar
+java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
+java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
 mkdir "output"
-antlr4 -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
-antlr4 -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
+mkdir "tests/output"
 python3 src/main.py input/y.c # y.c is the file we're going to compile
 
 #python3 src/main.py input/y.c
--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@
 
 # Asumes you're in the folder compilers
 #mkdir "tests/output"
+wget https://www.antlr.org/download/antlr-4.8-complete.jar
+java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
+java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
 mkdir "output"
-antlr4 -o gen -listener -visitor -Dlanguage=Python3 -lib input input/llvm.g4
-antlr4 -o gen_llvm -listener -visitor -Dlanguage=Python3 -lib input input/c.g4
 python3 src/main.py input/y.c # y.c is the file we're going to compile
