98f540069a1f28d179651f63dc4f117aeae60c14
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-23 15:03:55 +0200

    Forced antlr output not to create a subfolder input

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 wget https://www.antlr.org/download/antlr-4.8-complete.jar
-java -jar antlr-4.8-complete.jar -o gen -listener -visitor -Dlanguage=Python3 input/llvm.g4
-java -jar antlr-4.8-complete.jar -o gen_llvm -listener -visitor -Dlanguage=Python3 input/c.g4
+java -jar antlr-4.8-complete.jar -fo gen -listener -visitor -Dlanguage=Python3 input/llvm.g4
+java -jar antlr-4.8-complete.jar -fo gen_llvm -listener -visitor -Dlanguage=Python3 input/c.g4
 mkdir "output"
 mkdir "tests/output"
 python3 src/main.py input/y.c # y.c is the file we're going to compile
